e81d3f8b50a302fb06d1a0329f86db407e8d7fb3
Joost Schuur <jschuur@jschuur.com>
2023-05-06 12:54:56 +0100

    style: footer, bg color, title tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FlashCards from '~/components/FlashCards';
+import Footer from '~/components/Footer';
 
 export default function Home() {
   return (
-    <main className='flex flex-col justify-between px-20 py-6'>
-      <h1 className='pb-6 text-4xl font-bold text-center'>Mandarin Flashcards</h1>
+    <div className='flex flex-col justify-between px-20 py-8'>
+      <h1 className='pb-8 text-4xl font-bold text-center'>AI Generated Mandarin Flashcards</h1>
       {/* @ts-expect-error */}
       <FlashCards />
-    </main>
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className='py-6 text-sm font-light text-center'>
+      <div>
+        From <a href='https://joostschuur.com'>Joost Schuur</a> &middot;{' '}
+        <a href='https://twitter.com/joostschuur'>@joostschuur</a> &middot;{' '}
+        <a href='https://github.com/jschuur/ai-mandarin-flashcards'>GitHub</a>
+      </div>
+    </footer>
+  );
+}
