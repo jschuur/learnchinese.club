077a5987fa834eed86e81cb852794b5f97620fcc
Joost Schuur <jschuur@jschuur.com>
2023-05-10 22:19:21 +0100

    style: footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,14 @@ export default function Footer() {
   return (
     <footer className='py-6 text-sm font-light text-center'>
       <div>
-        From <a href='https://joostschuur.com'>Joost Schuur</a> &middot;{' '}
+        Powered <a href='https://joostschuur.com'>Joost Schuur</a> and ChatGPT &middot;{' '}
         <a href='https://twitter.com/joostschuur'>@joostschuur</a> &middot;{' '}
         <a href='https://github.com/jschuur/learnchinese.club'>GitHub</a>
       </div>
-      <div>Content powered by ChatGPT. Work in progress.</div>
+      <div>
+        Work in progress: HSK level generation needs improving, add filter by HSK level, add click
+        to reveal interface.
+      </div>
     </footer>
   );
 }
