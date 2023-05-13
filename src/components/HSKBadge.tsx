bb73446955a75e46f902069a8799779ab2f71c5b
Joost Schuur <jschuur@jschuur.com>
2023-05-13 11:41:29 +0100

    style: hsk badge color by difficulty

--- src/components/HSKBadge.tsx
+++ src/components/HSKBadge.tsx
@@ -3,8 +3,12 @@ type Props = {
 };
 
 export default function HSKBadge({ hskLevel }: Props) {
+  const color = hskLevel <= 2 ? 'bg-green-600' : hskLevel <= 4 ? 'bg-orange-500' : 'bg-red-600';
+
   return (
-    <span className='px-2 py-1 ml-auto mr-2 text-xs font-semibold tracking-wider text-white bg-green-600 border rounded-md whitespace-nowrap font- items-top'>
+    <span
+      className={`px-2 py-1 ml-auto mr-2 text-xs font-semibold tracking-wider text-white ${color} border rounded-md whitespace-nowrap font- items-top`}
+    >
       HSK {hskLevel}
     </span>
   );
